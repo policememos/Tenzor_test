f10998a5b4959ca255948ed03fc6de9e74df9e8a
homePoliceMemos <policememos@gmail.com>
2023-11-06 19:52:21 +0500

    fix 0.2

--- tenzor.py
+++
@@ -1,5 +0,0 @@
-from base_app import BasePage
-from selenium.webdriver.common.by import By
-
-class SbisLocators:
-    SBIS_CONTACTS_BUTTON = (By.CLASS_NAME, "sbisru-Header__menu-item sbisru-Header__menu-item-1 mh-8  s-Grid--hide-sm")
